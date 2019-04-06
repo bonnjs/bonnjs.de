75b2074c535cab61a4ef88ba148ff2370eb59090
sgarp <8928029+sgarp@users.noreply.github.com>
2019-04-06 16:21:14 +0200

    Add About Us and Events sections and extract Footer component

---
+++ src/components/about-us.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const AboutUs = () => {
+  return (
+    <>
+      <h2>Who we are</h2>
+      <p>
+        We are a user group for JavaScript developers in Bonn to learn and
+        discuss JavaScript. All JavaScript levels are welcome. You can meet
+        people, talk about JavaScript related technologies, find a mentor or
+        someone to do pair programming with, or just work on your own tutorials,
+        books, or projects. No matter if you are into Angular, React, jQuery,
+        Backbone, Ember, Meteor, whatever... Everybody is welcome!
+      </p>
+    </>
+  )
+}
+
+export default AboutUs
---
+++ src/components/events.js
@@ -0,0 +1,89 @@
+import React from "react"
+
+const DefaultText = () => {
+  return (
+    <div>
+      <p>
+        User group for JavaScript developers in Bonn to learn and discuss about
+        JavaScript. All JavaScript levels are welcome. You can meet people, talk
+        about JavaScript related technologies, join the FreeCodeCamp Bonn or
+        just work on your own tutorials, books, or projects.
+      </p>
+      &hellip;
+      {/* <p>
+        No matter if you are into Angular, React, jQuery, Backbone, Ember,
+        Meteor, or just don't what this is all about...
+      </p>
+      <p>Everybody is welcome!</p>
+
+      <p>
+        After our first successful meeting on 1st August 2018, we would like to
+        expand the variety of topics and prepared two talks.
+      </p>
+
+      <p>****</p>
+
+      <p>Agenda will follow in the next days!</p>
+
+      <p>Hope to see you!</p>
+
+      <p>Timo & Martin</p> */}
+    </div>
+  )
+}
+
+const events = [
+  {
+    url: "https://www.meetup.com/de-DE/BonnJS/events/qjskvqyzgbhc/",
+    date: "2019-04-25T19:00:00+02:00",
+    title: "Meetup BonnJS",
+    address: `Digital Hub Bonn
+Rheinwerkallee 6
+Bonn`,
+    description: "",
+  },
+  {
+    url: "https://www.meetup.com/de-DE/BonnJS/events/qjskvqyzgbhc/",
+    date: "2019-05-23T19:00:00+02:00",
+    title: "Meetup BonnJS",
+    address: `Digital Hub Bonn
+Rheinwerkallee 6
+Bonn`,
+    description: "",
+  },
+]
+
+const Events = () => {
+  return (
+    <>
+      <h2>Events</h2>
+      {events.map(({ date, title, address, url }) => {
+        const dateObject = new Date(date)
+        const dateString = dateObject.toLocaleDateString()
+        const timeString = dateObject.toLocaleTimeString()
+        dateObject.toString()
+        return (
+          <div className="event">
+            <time datetime={date}>
+              {dateString} {timeString}
+            </time>
+            <h3>
+              <a href={url}>{title}</a>
+            </h3>
+            <address>
+              {address.split(/$/m).map((line, i) => (
+                <>
+                  {i !== 0 && <br />}
+                  {line}
+                </>
+              ))}
+            </address>
+            <DefaultText />
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Events
